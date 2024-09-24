7f8f8d7245b0f3246eb70c45fa2d0d4f29af7ac7
Vera Kovalchuk <kovalchukvera86@yandex.ru>
2024-09-25 01:39:59 +0400

    create test for masks.py

--- src/masks.py
+++ src/masks.py
@@ -3,15 +3,19 @@ from typing import Union
 
 def get_mask_card_number(card_number: Union[str]) -> Union[str]:
     """Функция принимает на вход номер карты и возвращает ее маску"""
-    return f"{card_number[:4]} {card_number[4:6]} ** **** {card_number[-4:]}"
-
-
-print(get_mask_card_number("7000792289606361"))
+    if card_number.isdigit() and len(card_number) == 16:
+       return f"{card_number[:4]} {card_number[4:6]} ** **** {card_number[-4:]}"
+    else:
+        return 'Некорректный ввод данных'
+print(get_mask_card_number('7000792289606361'))
 
 
 def get_mask_account(account_number: Union[str]) -> Union[str]:
     """Функция принимает на вход номер счета в виде числа и возвращает маску номера"""
-    return f"**{account_number[-4:]}"
+    if account_number.isdigit() and len(account_number) == 20:
+        return f"**{account_number[-4:]}"
+    else:
+        return 'Некорректный ввод данных'
 
 
 print(get_mask_account("73654108430135874305"))
