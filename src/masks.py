d0e6a3f3e4617316d26cedd90ad815913589dd36
Vera Kovalchuk <kovalchukvera86@yandex.ru>
2024-09-26 08:20:43 +0400

    modify for testing masks

--- src/masks.py
+++ src/masks.py
@@ -4,9 +4,11 @@ from typing import Union
 def get_mask_card_number(card_number: Union[str]) -> Union[str]:
     """Функция принимает на вход номер карты и возвращает ее маску"""
     if card_number.isdigit() and len(card_number) == 16:
-       return f"{card_number[:4]} {card_number[4:6]} ** **** {card_number[-4:]}"
+        return f"{card_number[:4]} {card_number[4:6]} ** **** {card_number[-4:]}"
     else:
         return 'Некорректный ввод данных'
+
+
 print(get_mask_card_number('7000792289606361'))
 
 
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -7,7 +7,7 @@ from src.masks import get_mask_account, get_mask_card_number
                                                    ('', 'Некорректный ввод данных'),
                                                    ('6788gghg', 'Некорректный ввод данных')
                                                    ])
-def test_get_mask_card_number(card_number, expected):
+def test_get_mask_card_number(card_number: str, expected: str):
     """Тестирование маскировки номера карты"""
     assert get_mask_card_number(card_number) == expected
 
@@ -16,7 +16,7 @@ def test_get_mask_card_number(card_number, expected):
 @pytest.mark.parametrize('account_number, expected', [('73654108430135584305', '**4305'),
                                                       ('736541084301358', 'Некорректный ввод данных'),
                                                       ('', 'Некорректный ввод данных')])
-def test_get_mask_account(account_number, expected):
+def test_get_mask_account(account_number: str, expected: str):
     """Тестирование маскировки номера счета"""
     assert get_mask_account(account_number) == expected
 
