c011b7162be3b8e23596d5e7161ef0b16e4f8dfd
Vera Kovalchuk <kovalchukvera86@yandex.ru>
2024-11-01 23:40:06 +0400

    rewrite functions using loggers

--- src/transactions_csv_excel.py
+++ src/transactions_csv_excel.py
@@ -1,17 +1,83 @@
-import pandas as pd
+import csv
+import logging
+
+import openpyxl
+
+from src.utils import current_dir
+
+PATH_TO_CSV = current_dir / "files" / "transactions.csv"
+PATH_TO_EXCEL = current_dir / "files" / "transactions_excel.xlsx"
+
+logger = logging.getLogger("files")
+logger.setLevel(logging.INFO)
+fileHandler = logging.FileHandler(current_dir / "logs" / "files.log", encoding="UTF-8", mode="w")
+fileFormatter = logging.Formatter("%(asctime)s %(name)s %(levelname)s: %(message)s")
+fileHandler.setFormatter(fileFormatter)
+logger.addHandler(fileHandler)
 
 
-def transactions_csv():
-    """Функция для считывания финансовых операций из CSV"""
-    df = pd.read_csv('../transactions.csv', delimiter=';')
-    dict_data = df.to_dict(orient='records')
-    return dict_data
-#print(transactions_csv())
+def read_file_csv(file):
+    try:
+        logger.info("Получаем данные файла")
+        with open(file, "r", encoding="UTF-8") as f:
+            reader = csv.DictReader(f, delimiter=";")
 
-def transactions_excel():
-    """Функция для считывания финансовых операций из Excel"""
-    df = pd.read_excel('transactions_excel.xlsx')
-    dict_dada = df.to_dict(orient='records')
-    return dict_dada
-#print(transactions_excel())
+            filtered_data = []
+            for row in reader:
+                if row["id"] == "" or row["state"] == "" or row["date"] == "":
+                    continue
+                filtered_data.append(
+                    {
+                        "id": int(row["id"]),
+                        "state": row["state"],
+                        "date": row["date"],
+                        "operationAmount": {
+                            "amount": row["amount"],
+                            "currency": {"name": row["currency_name"], "code": row["currency_code"]},
+                        },
+                        "from": row.get("from", ""),
+                        "to": row.get("to", ""),
+                        "description": row["description"],
+                    }
+                )
+            return filtered_data
+    except Exception:
+        logger.error("Ошибка!")
+        return []
 
+
+def read_excel(file):
+    try:
+        transactions = []
+        workbook = openpyxl.load_workbook(file)
+        sheet = workbook.active
+        headers = [cell.value for cell in sheet[1]]
+
+        for row in sheet.iter_rows(min_row=2, values_only=True):
+            row_data = dict(zip(headers, row))
+            if row_data["id"] is None or row_data["state"] is None or row_data["date"] is None:
+                continue
+
+            transactions.append(
+                {
+                    "id": int(row_data["id"]),
+                    "state": row_data["state"],
+                    "date": row_data["date"],
+                    "operationAmount": {
+                        "amount": row_data["amount"],
+                        "currency": {"name": row_data["currency_name"], "code": row_data["currency_code"]},
+                    },
+                    "from": row_data.get("from", ""),
+                    "to": row_data.get("to", ""),
+                    "description": row_data["description"],
+                }
+            )
+        return transactions
+    except FileNotFoundError:
+        print(f"Файл не найден: {file}")
+        return []
+
+
+if __name__ == "__main__":
+    print(read_file_csv(PATH_TO_CSV))
+    # print(read_excel(PATH_TO_EXCEL))
