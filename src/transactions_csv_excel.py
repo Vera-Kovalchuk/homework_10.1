e4a56018784579faaa61dbd980f34b8f605d27cd
Vera Kovalchuk <kovalchukvera86@yandex.ru>
2024-11-01 21:36:02 +0400

    add module and 2 functions reading from csv and excel files

--- src/transactions_csv_excel.py
+++ src/transactions_csv_excel.py
@@ -2,12 +2,14 @@ import pandas as pd
 
 
 def transactions_csv():
+    """Функция для считывания финансовых операций из CSV"""
     df = pd.read_csv('../transactions.csv', delimiter=';')
     dict_data = df.to_dict(orient='records')
     return dict_data
 #print(transactions_csv())
 
 def transactions_excel():
+    """Функция для считывания финансовых операций из Excel"""
     df = pd.read_excel('transactions_excel.xlsx')
     dict_dada = df.to_dict(orient='records')
     return dict_dada
